ad7cec88ab3dcc1dd02c6115d29ab425f4a87a01
obj63mc <obj63mc@gmail.com>
2015-01-29 10:37:29 -0600

    initial files

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # memecanvas
 Node.js Meme Generator that uses Node-Canvas to render the final image.
+
+# To Install
+        $ npm install memecanvas
+
+# Requirements
+memecanvas utilizes Automattic's node-canvas module which has some pre-requisites that must be installed on your machine.  To find out more please check out their [Wiki](https://github.com/Automattic/node-canvas/wiki).
+
+# API
+
+* init(outputDirectoryName, fileAppendedName) <br/> The outputDirectoryName is where the meme file will be written to on your file system.<br/><br/>fileAppendedName is a string that will be appended to the original file name. eg - if fileAppendedName = '-meme', photo.png will write out to photo-meme.png
+* generate(file, topText, bottomText, callback)<br/>file is the filename and location - eg. __dirname + 'public/images/photo.png'<br/><br/>topText and bottomText are the text that will be overlayed on the top and bottom of the photo respectively.<br/> callback will return the filename or an error
+
+# Example Uses -
+    var memecanvas = require('memecanvas');
+    memecanvas.init('./tmp', '-meme');
+    memecanvas.generate('./photo.png', 'Top of Meme', 'Bottom of Meme', function(error, memefilename){
+        if(err){
+            console.log(error);
+        } else {
+            console.log(memefilename);
+        }
+    });
---
+++ index.js
@@ -0,0 +1,84 @@
+var image = require('./lib/image');
+var fs = require('fs');
+
+var outputDirectory = './tmp';
+var appendedFilename = '-meme';
+
+var endsWith = function(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+};
+
+var drawText = function(context, pos, text, width, height) {
+    var fontSize = 100;
+    context.textAlign = "center";
+    context.fillStyle = "#fff";
+    context.strokeStyle = "#000";
+    context.lineWidth = 6;
+
+    while(1) {
+        context.font = "bold " + fontSize + "px Impact";
+        if( (context.measureText(text).width < (width-15)) && (fontSize < height/10) ) {
+            break;
+        }
+        fontSize-=2;
+    }
+
+    var y;
+    if(pos == "top") {
+        y = fontSize + 15;
+    } else if(pos == "bottom") {
+        y = height - 15;
+    }
+
+    context.strokeText(text, width/2, y);
+    context.fillText(text, width/2, y);
+    return context;
+};
+
+var memecanvas = module.exports;
+
+memecanvas.init = function(output, append){
+    outputDirectory = output;
+    appendedFilename = append;
+};
+
+memecanvas.generate = function(file, topText, bottomText, next){
+    image.get(file, function(img){
+        if(img){
+            try{
+                var ctx = image.ctx(img.canvas.width, img.canvas.height);
+                ctx.drawImage(img.canvas, 0, 0);
+                ctx = drawText(ctx, 'top', topText, img.canvas.width, img.canvas.height);
+                ctx = drawText(ctx, 'bottom', bottomText, img.canvas.width, img.canvas.height);
+
+                var memefilename = file.split('/');
+                memefilename = memefilename[memefilename.length-1];
+
+                var parts = memefilename.split('.');
+                parts.pop();
+                parts.push(appendedFilename+'.png');
+
+                memefilename = parts.join('.');
+
+                if(!fs.existsSync(outputDirectory)){
+                    fsmkdirSync(outputDirectory);
+                }
+
+                if(!endsWith(outputDirectory, '/')){
+                    outputDirectory = outputDirectory + '/';
+                }
+
+                image.save(ctx, outputDirectory + memefilename, function() {
+                    ctx = null;
+                    return next(null, outputDirectory+memefilename);
+                });
+            } catch(e){
+                return next(e, null);
+            }
+
+        } else {
+            return ('could not open the image provided', null);
+        }
+
+    });
+};
